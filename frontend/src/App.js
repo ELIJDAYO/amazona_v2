78acaec1270947ada556021fd21c372ac417784a
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-03-06 07:16:14 +0800

    created signin screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SinginScreen';
 function App() {
   const { state } = useContext(Store);
   const { cart } = state;
@@ -37,6 +38,7 @@ function App() {
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
+              <Route path="/sigin" element={<SigninScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
