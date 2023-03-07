a8c5d22ac6db4f9a33ccecae175aaef4abd0a3de
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-03-07 16:36:11 +0800

    na

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import SigninScreen from './screens/SigninScreen';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -19,6 +20,7 @@ function App() {
     // type of action to dispatch
     ctxDispatch({ type: 'USER_SIGNOUT' });
     localStorage.removeItem('userInfo');
+    localStorage.removeItem('shippingAddress');
   };
   return (
     <BrowserRouter>
@@ -72,6 +74,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/shipping" element={<ShippingAddressScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
---
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -0,0 +1,117 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet-async';
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import { Store } from '../Store';
+import { Navigate, useNavigate } from 'react-router-dom';
+import CheckoutSteps from '../components/CheckoutSteps';
+export default function ShippingAddressScreen() {
+  const navigate = useNavigate();
+
+  //   after this go to app.js to add route
+  const [country, setCountry] = useState('');
+  //   after thus go to store.js to add
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  //   read from the state, no need to refill the form, get data from cart
+  const {
+    userInfo,
+    cart: { shippingAddress },
+  } = state;
+  // if shippingAddress.data exists use it
+  const [fullName, setFullName] = useState(shippingAddress.fullName || '');
+  const [address, setAddress] = useState(shippingAddress.address || '');
+  const [city, setCity] = useState(shippingAddress.city || '');
+  const [postalCode, setPostalCode] = useState(
+    shippingAddress.postalCode || ''
+  );
+  useEffect(() => {
+    if (!userInfo) {
+      navigate('/signin?redirect=/shipping');
+    }
+  }, [userInfo, navigate]);
+  const submitHandler = (e) => {
+    // dont refresh the page after submit
+    e.preventDefault();
+    ctxDispatch({
+      // new action is type
+      type: 'SAVE_SHIPPING_ADDRESS',
+      //   from the text boxes
+      payload: {
+        fullName,
+        address,
+        city,
+        postalCode,
+        country,
+      },
+    });
+    localStorage.setItem(
+      'shippingAddress',
+      JSON.stringify({
+        fullName,
+        address,
+        city,
+        postalCode,
+        country,
+      })
+    );
+    navigate('/payment');
+  };
+  return (
+    <div>
+      <Helmet>
+        <title>Shipping Address</title>
+      </Helmet>
+      <CheckoutSteps step1 step2></CheckoutSteps>
+      <div className="container small-container">
+        <h1 className="my-3">Shipping Address</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="mb-3" controlId="fullName">
+            <Form.Label>Full Name</Form.Label>
+            <Form.Control
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="address">
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="city">
+            <Form.Label>City</Form.Label>
+            <Form.Control
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="postalCode">
+            <Form.Label>Postal Code</Form.Label>
+            <Form.Control
+              value={postalCode}
+              onChange={(e) => setPostalCode(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="country">
+            <Form.Label>Country</Form.Label>
+            <Form.Control
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <div className="mb-3">
+            <Button variant="primary" type="submit">
+              Continue
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
+}
