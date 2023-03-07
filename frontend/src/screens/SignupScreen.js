d49c80df64f457d76ec1ad342027c1b6d7be8d43
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-03-07 16:35:56 +0800

    Created shipping screen

---
+++ frontend/src/screens/SignupScreen.js
@@ -0,0 +1,84 @@
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+import { useContext, useEffect, useState } from 'react';
+import Axios from 'axios';
+
+import { Store } from '../Store';
+import { getError } from '../utils';
+import { toast, ToastContainer } from 'react-toastify';
+
+export default function SigninScreen() {
+  const navigate = useNavigate();
+  const { search } = useLocation();
+  const redirectInUrl = new URLSearchParams(search).get('redirect');
+  const redirect = redirectInUrl ? redirectInUrl : '/';
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { userInfo } = state;
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    // To prevent refreshing this page, when user click on Sign In Button, then create a try catch block.
+    try {
+      // pass credential to the api
+      const { data } = await Axios.post('/api/users/signin', {
+        email,
+        password,
+      });
+      // console.log(data);
+      // data from the backend, then go to Store.js
+      ctxDispatch({ type: 'USER_SIGNIN', payload: data });
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      navigate(redirect || '/');
+    } catch (err) {
+      toast.error(getError(err));
+    }
+  };
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+
+  return (
+    <Container className="small-container">
+      <Helmet>
+        <title>Sign In</title>
+      </Helmet>
+      {/*create margin from top and bottom two or three rem */}
+      <h1 className="my-3">Sign In</h1>
+
+      <Form onSubmit={submitHandler}>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            required
+            onChange={(e) => setEmail(e.target.value)} //value in the text box
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <div className="mb-3">
+          <Button type="submit">Sign In</Button>
+        </div>
+        <div className="mb-3">
+          New customer?{' '}
+          <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
+        </div>
+      </Form>
+    </Container>
+  );
+}
