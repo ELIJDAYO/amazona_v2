3810a1f16e383b2d7284e8ec21ea40293e7f6f22
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-03-11 09:02:27 +0800

    added workflow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,7 +32,7 @@ function App() {
           <Navbar bg="dark" variant="dark">
             <Container>
               <LinkContainer to="/">
-                <Navbar.Brand>amazona</Navbar.Brand>
+                <Navbar.Brand>Template</Navbar.Brand>
               </LinkContainer>
               <Nav className="me-auto">
                 <Link to="/cart" className="nav-link">
